f8b482f20f4c3322e4bb4f04328e8f23cb68d388
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-04-11 17:45:44 -0500

    Fix linters

--- person.rb
+++ person.rb
@@ -10,17 +10,16 @@ class Person < Nameable
     @id = rand(0..1000)
   end
 
-  attr_accessor :name
-  attr_accessor :age
+  attr_accessor :name, :age
   attr_reader :id
 
   # Methods
   def of_age?
-    return @age >= 18
+    @age >= 18
   end
 
   def can_use_services?
-    return @age >= 18 || @parent_permission
+    @age >= 18 || @parent_permission
   end
 
   def correct_name
@@ -32,7 +31,7 @@ end
 
 person = Person.new(22, 'maximilianus')
 puts person.correct_name
-puts capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name
-puts capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.correct_name
+puts capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+puts capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
