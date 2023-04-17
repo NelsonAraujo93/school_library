863d614ecd78e9500b4df4761701d0862451bf63
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-17 14:56:10 -0600

    Change input getter method default parameter as false

--- app.rb
+++ app.rb
@@ -16,14 +16,14 @@ class App
     }
   end
 
-  def input_getter(msj, is_number = true)
+  def input_getter(msj, is_number: false)
     print msj
-    value = is_number ? get.chomp.to_i : gets.chomp
+    value = is_number ? gets.chomp.to_i : gets.chomp
   end
 
   def print_add_book
-    title = input_getter('Title: ', false)
-    author = input_getter('Author: ', false)
+    title = input_getter('Title: ')
+    author = input_getter('Author: ')
     add_book(title, author)
   end
 
